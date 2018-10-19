5412745bdbf9e041f621c54ed18e6b1645263242
sasoripathos <sasori.pan.jerry@gmail.com>
2018-10-19 16:28:42 -0400

    Add logout controller

---
+++ code/wecare/src/main/java/com/newcomer/controller/LogoutController.java
@@ -0,0 +1,24 @@
+package com.newcomer.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LogoutController {
+
+	/**
+	 * User logout
+	 * @param request the HTTP request for logout.
+	 * @return the view's name of welcome page.
+	 */
+	@RequestMapping("/logout")
+	public String logout(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		session.invalidate();
+		System.out.println("You have logout!");
+		return "index";
+	}
+}
