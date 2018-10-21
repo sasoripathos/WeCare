324d3fc2e28974973b07a4d3a87d7b2114a3a068
sasoripathos <sasori.pan.jerry@gmail.com>
2018-10-20 21:30:54 -0400

    Add simple file upload request mapping
    Add a request handler in DashBoardController class and created a simple page for upload result.

--- code/wecare/src/main/java/com/newcomer/controller/DashBoardController.java
+++ code/wecare/src/main/java/com/newcomer/controller/DashBoardController.java
@@ -1,12 +1,17 @@
 package com.newcomer.controller;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.newcomer.dbservice.UserRepository;
 import com.newcomer.entity.User;
@@ -36,6 +41,20 @@ public class DashBoardController {
 		// Load login user to dashboard
 		model.addAttribute("loginUser", this.user);
 		return "dashboard";
+	}
+	
+	@PostMapping("/upload")
+	public String fileUpload(@RequestParam("file1") MultipartFile file) {
+		byte[] content = new byte[100];
+		try {
+			int a = file.getInputStream().read(content);
+			System.out.println("Read: " + a + " bytes");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Something is wrong");
+		}
+		return "upload_result";
 	}
 
 }
---
+++ code/wecare/src/main/webapp/WEB-INF/views/upload_result.jsp
@@ -0,0 +1,24 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>weCare</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<style type="text/css">
+	
+	</style>
+</head>
+<body>
+	<div class="nav">
+		<nav class="navbar navbar-default" role="navigation" style="margin-top:0; background-color:#A1CAF1">
+			<ul class="nav navbar-nav" style="margin-left: 87%">
+				<li><a href="/">weCare!</a></li>
+				<li style="background-color: #FFBF00;"><a href="/logout">logout</a></li>
+  			</ul>
+		</nav>
+	</div>
+	<p>File uploaded!</p>
+</body>
+</html>
