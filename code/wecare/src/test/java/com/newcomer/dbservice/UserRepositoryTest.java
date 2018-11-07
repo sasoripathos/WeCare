7db2f8cedc5868f65fe0d55613e0000ae730ad50
sasoripathos <sasori.pan.jerry@gmail.com>
2018-11-06 20:40:14 -0500

    Add embeded mongoDB for testing

---
+++ code/wecare/src/test/java/com/newcomer/dbservice/UserRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.newcomer.dbservice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.newcomer.entity.User;
+import com.newcomer.wecare.App;
+
+@RunWith(SpringRunner.class)
+@TestPropertySource(locations="classpath:test.properties")
+@SpringBootTest(classes = App.class, webEnvironment = WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration
+public class UserRepositoryTest {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	@Autowired
+	private MongoTemplate db;
+	
+	@Before
+	public void init() {
+		User a = new User("Jack", "jack@email.com", "UTSC", "12345", "DEF");
+		repo.save(a);
+	}
+	
+	@Test
+	public void testCollectionExist() {
+		assertTrue(db.collectionExists("User"));
+	}
+
+	@Test
+	public void testSaveUser() {
+		User a = new User("Jason", "test@email.com", "Agence", "12345", "ABC");
+		assertTrue(repo.save(a) != null);
+	}
+	
+	@Test
+	public void testCanFindUser() {
+		User b = repo.findByEmail("jack@email.com");
+		assertEquals(b.getEmail(), "jack@email.com");
+	}
+	
+	@Test
+	public void testCannotFindUser() {
+		User b = repo.findByEmail("alice@gmail.com");
+		assertTrue(b == null);
+	}
+}
