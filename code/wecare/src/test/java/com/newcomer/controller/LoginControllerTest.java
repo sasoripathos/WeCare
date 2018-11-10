36e4b9e4a08d17834822d90d28fbe075e3f9971f
sasoripathos <sasori.pan.jerry@gmail.com>
2018-11-10 18:48:06 -0500

    Finish login controller tests

--- code/wecare/src/test/java/com/newcomer/controller/LoginControllerTest.java
+++ code/wecare/src/test/java/com/newcomer/controller/LoginControllerTest.java
@@ -4,6 +4,8 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import javax.servlet.http.HttpSession;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -34,7 +37,7 @@ public class LoginControllerTest {
 	
 	@Autowired
 	private MockMvc mockmvc;
-	
+
 	@Before
 	public void init() {
 		User a = new User("Amy", "amy@gmail.com", "Agence", "12345", "ABC");
@@ -61,19 +64,20 @@ public class LoginControllerTest {
 	@Test
 	public void testLoginAgencyUserSuccess() throws Exception {
 		User b = new User();
-		mockmvc.perform(post("/login")
+		HttpSession session = mockmvc.perform(post("/login")
 				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
 				.param("email", "amy@gmail.com")
 				.param("password", "12345")
 				.sessionAttr("loginUser", b))
 			.andExpect(status().is3xxRedirection())
 			.andExpect(view().name("redirect:/dashboard"))
-			/*.andExpect(model().attribute("loginUser", hasProperty("id", notNullValue())))
+			.andReturn().getRequest().getSession();
+		mockmvc.perform(get("/dashboard").session((MockHttpSession) session))
+			.andExpect(status().isOk())
 			.andExpect(model().attribute("loginUser", hasProperty("name", is("Amy"))))
 			.andExpect(model().attribute("loginUser", hasProperty("email", is("amy@gmail.com"))))
-			.andExpect(model().attribute("loginUser", hasProperty("role", is("Agence"))))
-			.andExpect(model().attribute("loginUser", hasProperty("password", is("12345"))))
-			.andExpect(model().attribute("loginUser", hasProperty("agency", is("ABC"))))*/;
+			.andExpect(model().attribute("loginUser", hasProperty("role", is("Agence"))));
+		
 	}
 	
 	@Test
@@ -85,13 +89,70 @@ public class LoginControllerTest {
 				.param("password", "54321")
 				.sessionAttr("loginUser", b))
 			.andExpect(status().is3xxRedirection())
-			.andExpect(view().name("redirect:/login"))
-			/*.andExpect(model().attribute("loginUser", hasProperty("id", nullValue())))
-			.andExpect(model().attribute("loginUser", hasProperty("name", nullValue())))
-			.andExpect(model().attribute("loginUser", hasProperty("email", nullValue())))
-			.andExpect(model().attribute("loginUser", hasProperty("role", nullValue())))
-			.andExpect(model().attribute("loginUser", hasProperty("password", nullValue())))
-			.andExpect(model().attribute("loginUser", hasProperty("agency", nullValue())))*/;
+			.andExpect(view().name("redirect:/login"));
+		
+	}
+	
+	@Test
+	public void testLoginTEQUserSuccess() throws Exception {
+		User b = new User();
+		HttpSession session = mockmvc.perform(post("/login")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("email", "boris@gmail.com")
+				.param("password", "65432")
+				.sessionAttr("loginUser", b))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(view().name("redirect:/dashboard"))
+			.andReturn().getRequest().getSession();
+		
+		mockmvc.perform(get("/dashboard").session((MockHttpSession) session))
+			.andExpect(status().isOk())
+			.andExpect(model().attribute("loginUser", hasProperty("name", is("Boris"))))
+			.andExpect(model().attribute("loginUser", hasProperty("email", is("boris@gmail.com"))))
+			.andExpect(model().attribute("loginUser", hasProperty("role", is("TEQ"))));
+	}
+	
+	@Test
+	public void testLoginTEQUserFail() throws Exception {
+		User b = new User();
+		mockmvc.perform(post("/login")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("email", "boris@gmail.com")
+				.param("password", "54321")
+				.sessionAttr("loginUser", b))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(view().name("redirect:/login"));
+	}
+	
+	@Test
+	public void testLoginUTSCUserSuccess() throws Exception {
+		User b = new User();
+		HttpSession session = mockmvc.perform(post("/login")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("email", "cindy@gmail.com")
+				.param("password", "98765")
+				.sessionAttr("loginUser", b))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(view().name("redirect:/dashboard"))
+			.andReturn().getRequest().getSession();
+		
+		mockmvc.perform(get("/dashboard").session((MockHttpSession) session))
+			.andExpect(status().isOk())
+			.andExpect(model().attribute("loginUser", hasProperty("name", is("Cindy"))))
+			.andExpect(model().attribute("loginUser", hasProperty("email", is("cindy@gmail.com"))))
+			.andExpect(model().attribute("loginUser", hasProperty("role", is("UTSC"))));
+	}
+	
+	@Test
+	public void testLoginUTSCUserFail() throws Exception {
+		User b = new User();
+		mockmvc.perform(post("/login")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("email", "cendy@gmail.com")
+				.param("password", "98765")
+				.sessionAttr("loginUser", b))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(view().name("redirect:/login"));
 	}
 	
 	@After
